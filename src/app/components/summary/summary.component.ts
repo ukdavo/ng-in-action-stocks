5f656fdc92ed7b7e239ae60c51b15cbd90ee18ca
ukdavo <ukdavo@googlemail.com>
2018-09-21 11:28:26 +0100

    Added missing files

---
+++ src/app/components/summary/summary.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'summary',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.css']
+})
+export class SummaryComponent {
+
+  @Input()
+  stock: any;
+
+  constructor() { }
+
+  isNegative() {
+    return (this.stock && this.stock.change < 0);
+  }
+
+  isPositive() {
+    return (this.stock && this.stock.change > 0);
+  }
+}
